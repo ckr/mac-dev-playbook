e297c4e916bb6356951cae531e0e31880d982ea7
Constantinos Kouloumbris <constantinoskouloumbris@rentalcars.com>
2017-05-24 15:47:33 +0100

    added kubernetes cli and disabled neofetch options to make it faster

--- roles/terminal/files/heimdall/heimdall.plugin.zsh
+++ roles/terminal/files/heimdall/heimdall.plugin.zsh
@@ -1,2 +1,2 @@
 # Show heimdall logo when terminal opens
-neofetch --ascii "${ZSH_CUSTOM}/plugins/heimdall/heimdall-ascii" --ascii_colors 5  --color_blocks off
+neofetch --ascii "${ZSH_CUSTOM}/plugins/heimdall/heimdall-ascii" --ascii_colors 5  --color_blocks off --disable memory resolution packages
